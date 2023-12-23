56bab38f69de074fbff0ba97bb214d11186a5132
jim3692 <jim3692@gmail.com>
2023-12-24 00:15:53 +0200

    Fix parsing errors

--- can-decoder-python-cantools/can_decoder-chris.py
+++ can-decoder-python-cantools/can_decoder-chris.py
@@ -78,7 +78,6 @@ def can_decoder(
                 log_data[previous_datetime_str] = allMessages
                 with open(f"{decoded_output_jsons_folder}/{previous_datetime_str}.json", 'w') as json_content:
                     json.dump(log_data, json_content, indent=4)
-                print("file saved")
                 log_data = {}
                 allMessages = []
             except Exception as e:
@@ -91,13 +90,13 @@ def can_decoder(
         can_data = line.split(';')[3].strip()
 
         try:
+            if can_id_int not in db._frame_id_to_message:
+                continue
+
             message = db.decode_message(can_id_int, bytearray.fromhex(can_data))
-            message_json = json.dumps(message)
-            message_dict = json.loads(message_json)
-            # allMessages.append(json.dumps(str(message)))
             allMessages.extend(
-                json.dumps({"canbus_id": can_id_str,"canbus_id_name": key, "value": value})
-                for key, value in message_dict.items()
+                {"canbus_id": can_id_str,"canbus_id_name": key, "value": str(value)}
+                for key, value in message.items()
             )
             # print("Decoded Message:", json.dumps(message))
         except ValueError as ve:
@@ -106,88 +105,4 @@ def can_decoder(
         except Exception as e:
             print(f"Error: Unable to decode message for ID {can_id_str}. {e}")
 
-        # print(str(i) + " Use this line: " + line.strip() 
-        #       + " | datetime_str: " + datetime_str
-        #       + " | can_id_str: " + can_id_str
-        #       + " | can_id_int: " + str(can_id_int)
-        #       + " | can_id_hex: " + str(can_id_hex)
-        #       + " | can_data: " + can_data
-        #       + " | message: " + str(message))
-        
-
-    # for i,line in enumerate(lines):
-    #     if(line.startswith("header:")):
-    #         # Extracting data in decimal format
-    #         words=lines[i+11].rstrip().split('[')
-    #         datastr=words[1].rstrip().split(']')[0]
-
-    #         #extracting seq
-    #         words=lines[i+1].rstrip().split()
-    #         seq=int(words[1])
-
-    #         #extracting time
-    #         words1=lines[i+3].rstrip().split()
-    #         words=lines[i+4].rstrip().split()
-    #         time=float(words1[1]+"."+words[1])
-
-    #         #extracting id
-    #         words=lines[i+6].rstrip().split()
-    #         mid=int(words[1])
-
-    #         #extracting if it is error
-    #         words=lines[i+9].rstrip().split()
-    #         error=(words[1]=='True')
-
-    #         #appending the data to an array
-    #         if i==0:
-    #             table=np.array([seq,time,mid,error,datastr])
-    #         else:
-    #             table=np.vstack((table,[seq,time,mid,error,datastr]))
-
-    # print("++++++++++++(++++++++=++++++++++++Raw Data++++++++++=+++++++++++++++++++++++++++++++++++++")
-    # print("Seq","\t","Time","\t","Id","\t","is_error","\t","Data")
-    # print(table)
-    # np.save("can0_rec1_rostopic_raw",table)
-    # print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-
-    # #--------------Decoding the can messages to phycial messages-------------------------------------------
-    # flag=0
-    # for i in range(len(table)):
-    #     print ("in table")
-    #     m_id=int(table[i,ref['id']])
-    #     if (m_id!=688 and m_id!=1200 and m_id!= 544):
-    #         print("continue")
-    #         continue
-    #     msg_data=table[i,ref['data']].split(',')
-    #     data_hex=""
-
-    #     for subdata in msg_data:
-    #         data_hex+=str(format(int(subdata),'02x'))
-    #     if flag==0:
-    #         decoded_table=np.array([float(table[i,ref['time']]),db.decode_message(int(table[i,ref['id']]),bytearray.fromhex(data_hex))])
-    #         flag=1
-
-    #         print ("in flag 0")
-    #         print("Time","\t","Physical Message")
-    #         print(decoded_table)
-    #     else:
-    #         decoded_table=np.vstack((decoded_table,[float(table[i,ref['time']]),db.decode_message(int(table[i,ref['id']]),bytearray.fromhex(data_hex))]))        
-
-    #         print ("in flag 1")
-    #         print("Time","\t","Physical Message")
-    #         print(decoded_table)
-    # #saving the decoded file
-    # # np.savetxt("can0_rec1_rostopic_decoded",decoded_table)
-    # save_file_path=de_file+".txt"
-    # fileh=open(save_file_path,"w")
-    # for i in range(len(decoded_table)):
-    #     if(i ==0):
-    #         fileh.write("Time (seconds)"+"\t\t"+"Phyical Message\n")
-    #     fileh.write(str(decoded_table[i,0])+"\t"+str(decoded_table[i,1])+"\n")
-    # fileh.close()
-    # print("Decoded can messages saved to file: {save_file_path}")
-
 can_decoder()
-
-str=json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
-print(str)
