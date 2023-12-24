e3e8d6fc0c0b4a518dbea4736fcf5a053b850eec
jim3692 <jim3692@gmail.com>
2023-12-24 15:48:48 +0200

    Refactor convert-clx000-to-c.c

--- clx000-log-to-c-struct/convert-clx000-to-c.c
+++ clx000-log-to-c-struct/convert-clx000-to-c.c
@@ -2,68 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
+int timeTokenToMillis (char * token) {
+  int tokenIdx = 0;
+  while (token[tokenIdx++] != 'T');
+
+  int timeHours = atoi((char[]) { token[tokenIdx++], token[tokenIdx++], '\0' });
+  int timeMinutes = atoi((char[]) { token[tokenIdx++], token[tokenIdx++], '\0' });
+  int timeSeconds = atoi((char[]) { token[tokenIdx++], token[tokenIdx++], '\0' });
+  int timeMillis = atoi((char[]) { token[tokenIdx++], token[tokenIdx++], token[tokenIdx++], '\0' });
+
+  return
+    timeMillis +
+    timeSeconds * 1000 +
+    timeMinutes * 1000 * 60 +
+    timeHours * 1000 * 60 * 60;
+}
+
 int main () {
   char *line = NULL;
-  size_t size;
-
-  char formattedTime[64];
-
-  char s_timeHours[3] = { 0 };
-  char s_timeMinutes[3] = { 0 };
-  char s_timeSeconds[3] = { 0 };
-  char s_timeMillis[4] = { 0 };
-
+  size_t size = 0;
   while (getline(&line, &size, stdin) > -1) {
     if (line[0] < '0' || line[0] > '9') continue;
 
     int lineIdx = 0;
-    while (line[lineIdx++] != 'T');
-
-    strncpy(s_timeHours, &line[lineIdx], 2);
-    lineIdx += 2;
-    strncpy(s_timeMinutes, &line[lineIdx], 2);
-    lineIdx += 2;
-    strncpy(s_timeSeconds, &line[lineIdx], 2);
-    lineIdx += 2;
-    strncpy(s_timeMillis, &line[lineIdx], 3);
-    lineIdx += 3;
-
-    int timeHours = atoi(s_timeHours);
-    int timeMinutes = atoi(s_timeMinutes);
-    int timeSeconds = atoi(s_timeSeconds);
-    int timeMillis = atoi(s_timeMillis);
-    int time = timeMillis
-      + 1000 * timeSeconds
-      + 1000 * 60 * timeMinutes
-      + 1000 * 60 * 60 * timeHours;
-
-    lineIdx++;
-
-    int isExtended = line[lineIdx] == '0' ? 0 : 1;
-    lineIdx += 1;
-
-    lineIdx++;
-
-    int idBegin = lineIdx;
-    while (line[++lineIdx] != ';');
-    int idEnd = lineIdx;
-    char s_id[5] = { 0 };
-    strncpy(s_id, &line[idBegin], idEnd - idBegin);
-
-    lineIdx++;
-
-    int dataBegin = lineIdx;
     while (line[++lineIdx] != '\n');
-    int dataEnd = lineIdx;
+    line[lineIdx] = '\0';
+
+    char *token = strtok(line, ";");
+
+    int time = timeTokenToMillis(token);
+    token = strtok(NULL, ";");
+
+    int isExtended = token[0] == '0' ? 0 : 1;
+    token = strtok(NULL, ";");
+
+    char s_id[5] = { 0 };
+    strcpy(s_id, token);
+    unsigned long long id = strtoll(s_id, NULL, 16);
+    token = strtok(NULL, ";");
+
     char s_data[17] = { 0 };
-    strncpy(s_data, &line[dataBegin], dataEnd - dataBegin);
+    strcpy(s_data, token);
     unsigned long long data = strtoll(s_data, NULL, 16);
 
-    printf(", { .time = %d, .isExtended = %d, .id = 0x%s, .size = %d, .data = 0x%016llx }\n",
+    printf(", { .time = %012d, .isExtended = %d, .id = 0x%04x, .size = %d, .data = 0x%016llx }\n",
            time,
            isExtended,
-           s_id,
-           (dataEnd - dataBegin) / 2,
+           id,
+           strlen(s_data) >> 1,
            data);
   }
 }
